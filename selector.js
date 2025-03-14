1e79dd41f2123d03adb9fbf02eaeffadd85920d8
ttngu105 <50807331+ttngu105@users.noreply.github.com>
2025-03-14 01:12:01 -0500

    Update selector.js

--- selector.js
+++ selector.js
@@ -1,2 +1,2 @@
 var people = ["Emily", "Tommy","Miles"];
-document.getElementById("demo").innerHTML = "<body>people[0]</body>";
+document.getElementById("demo").innerHTML = "<body>"+people[0]+"</body>";

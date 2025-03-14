c68ce72dbda90a2800e840efcef3efff33c63ecf
ttngu105 <50807331+ttngu105@users.noreply.github.com>
2025-03-14 15:36:02 -0500

    Update selector.js

--- selector.js
+++ selector.js
@@ -1,3 +1,12 @@
 var people = ["Emily", "Tommy","Miles"];
 document.getElementById("demo").innerHTML = "<h2>"+people[0]+"<h2>";
 document.write(5 + 6)
+function myFunction() {
+    console.log("Button clicked!");
+    if (paidpeople.length  >= people.length)
+    {paidpeople = []}
+    console.log(people[paidpeople.length])
+    person = people[paidpeople.length];
+    paidpeople.push(person);
+    document.getElementById("demo").innerHTML  = ("<h2>It is "+person+"'s turn<h2>");
+  }
